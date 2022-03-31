75288f05911b8d5eb5b29a6097d16d918d0b4a1f
amina <dahmouniamina.contact@gmail.com>
2022-03-31 17:12:43 +0000

    Spring Boot Spring MVC Thymeleaf Spring Data Intellij Part2

--- Patient-MVC/src/main/java/ma/enset/patientmvc/Entities/Patient.java
+++ Patient-MVC/src/main/java/ma/enset/patientmvc/Entities/Patient.java
@@ -2,17 +2,26 @@ package ma.enset.patientmvc.Entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty @Size(min = 4,max = 40)
     private String nom;
     @Temporal(TemporalType.DATE)
+    //Pour la récuperation de date
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private  boolean malade;
+    @DecimalMin("100")
     private int score;
 }
 
--- Patient-MVC/src/main/java/ma/enset/patientmvc/Web/PatientController.java
+++ Patient-MVC/src/main/java/ma/enset/patientmvc/Web/PatientController.java
@@ -6,9 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 import java.util.List;
 @Controller
 @AllArgsConstructor
@@ -31,6 +35,7 @@ public class PatientController {
         patientRepository.deleteById(id);
         return "redirect:/index?page="+page+"&keyword="+keyword;
     }
+
     @GetMapping("/")
     public  String home(){
         return "redirect:/index";
@@ -42,6 +47,32 @@ public class PatientController {
     @ResponseBody
     public List<Patient> listPatients(){
         return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return  "formPatients";
+    }
+    @PostMapping (path = "/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "")String keyword){
+      if (bindingResult.hasErrors()) return "formPatients";
+      patientRepository.save(patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+      return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,Long id,String keyword,int page){
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if (patient==null) throw new RuntimeException("Patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return  "editPatient";
     }
 
 }
